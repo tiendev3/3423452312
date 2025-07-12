4e8f604b342300b462ffa509af1d39d62dced4c3
tiendev3 <sandrahillundecent@umeharasyo.com>
2025-07-12 13:26:23 +0700

    Update keyvip.py

--- keyvip.py
+++ keyvip.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import requests
 
 # URL của file vipkey.json (dạng txt): mỗi dòng key|dd-mm-yyyy
-VIP_KEY_URL = "https://raw.githubusercontent.com/tiendev3/3423452312/main/vipkey.txt"
+VIP_KEY_URL = "https://raw.githubusercontent.com/tiendev3/23556463423332/refs/heads/main/vipkey.json"
 SECRET_KEY = "super-secret-key-tiendev"  # có thể đổi hoặc lấy từ env nếu muốn bảo mật hơn
 
 # Đọc file VIP key từ GitHub
